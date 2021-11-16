a5b7048311e6c35bcc52488f9fa581415e7301ff
ASavka <anton.savka@gmail.com>
2021-11-16 17:16:07 +0200

    Added parser of  big json in body

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   next();
 });
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 app.get('/', (req: Request, res: Response): void => {
   res.send('I am empty page');
